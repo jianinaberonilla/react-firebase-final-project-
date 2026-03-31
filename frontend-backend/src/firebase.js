15845a2b6ec691f3a095e612f1f4b4010b20a886
Jianina Beronilla <beronilla.jianina121105@gmail.com>
2026-03-31 09:53:16 +0800

    Update app to include student form and firestore list

--- frontend-backend/src/firebase.js
+++ frontend-backend/src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzPm3oHvaRsfS3ZZADxscVY42iubsl6n0",
-  authDomain: "notes-app-6f9ea.firebaseapp.com",
-  projectId: "notes-app-6f9ea",
-  storageBucket: "notes-app-6f9ea.firebasestorage.app",
-  messagingSenderId: "52825671843",
-  appId: "1:52825671843:web:0f61ad60a4e08d314bd716"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
